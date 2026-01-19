4749d94680d92a163c8276b8c1c3addab4c3e5ac
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2026-01-19 20:24:04 +0900

    Add | Vertex struct

--- TigerEngine/Project/Base/Datas/Vertex.h
+++ TigerEngine/Project/Base/Datas/Vertex.h
@@ -1,17 +1,6 @@
 #pragma once
 #include "../pch.h"
 
-/*
-struct Vertex
-{
-    Vector3 position;
-    Vector2 uv;
-    Vector3 tangent;
-    Vector3 biTangent;
-    Vector3 normal;
-};
-*/
-
 struct BoneWeightVertexData
 {
     Vector3 position;
@@ -39,4 +28,28 @@ struct BoneWeightVertexData
 			}
 		}
 	}
+};
+
+
+// Only Position
+struct Position_Vertex
+{
+    Vector3 position;
+};
+
+// Particle Quad
+struct ParticleQuadVertex
+{
+    Vector2 corner; // (-0.5 ~ 0.5) : 빌보드용 로컬 오프셋
+    Vector2 uv;
+};
+
+// ParticleInstance
+struct ParticleInstance
+{
+    Vector3 pos;
+    float   rotation;
+    Vector2 size;
+    float   frame;
+    Vector4 color;
 };
